8849b07ff4a751a0d69c5826175feae9bc4c7f10
Jonas <jonas@Jonass-MacBook-Air.local>
2025-03-23 18:43:21 +0200

    cors

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://ussimo-dashboard.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
